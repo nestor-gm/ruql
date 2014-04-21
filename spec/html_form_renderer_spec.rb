ebd35a37f9a99b799a55931c1825079b7ff3b8c7
Juan José <jjlabradorglez@gmail.com>
2014-04-21 22:48:09 +0100

    Fixing tests

--- spec/html_form_renderer_spec.rb
+++ spec/html_form_renderer_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+ENV['environment'] = 'test'
 
 describe HtmlFormRenderer do
   describe 'when created' do
@@ -11,11 +12,11 @@ describe HtmlFormRenderer do
     end
     it 'should include CSS link with -c option' do
       rendering_with('c' => 'foo.html').
-        should match /<link rel="stylesheet" type="text\/css" href="#{File.expand_path('foo.html')}"/
+        should match /<link rel=stylesheet type=text\/css href=#{File.expand_path('foo.html')}/
     end
     it 'should include CSS link with --css option' do
       rendering_with('css' => 'foo.html').
-      should match /<link rel="stylesheet" type="text\/css" href="#{File.expand_path('foo.html')}"/
+      should match /<link rel=stylesheet type=text\/css href=#{File.expand_path('foo.html')}/
     end
     it 'should use ERB template if directed' do
       rendering_with('template' => File.join(File.dirname(__FILE__),'fixtures','template.html.erb')).
@@ -29,14 +30,14 @@ describe HtmlFormRenderer do
     end
     it 'should include JS link with -j option' do
       rendering_with('j' => 'foo.js').
-      should match /<script type="text\/javascript" src="#{File.expand_path('foo.js')}"/
+      should match /<script type=text\/javascript src=#{File.expand_path('foo.js')}/
     end
     it 'should include JS link with --js option' do
       rendering_with('js' => 'foo.js').
-      should match /<script type="text\/javascript" src="#{File.expand_path('foo.js')}"/
+      should match /<script type=text\/javascript src=#{File.expand_path('foo.js')}/
     end
   end
-=begin
+
   describe 'local variable' do
     require 'tempfile'
     def write_template(str)
@@ -47,18 +48,18 @@ describe HtmlFormRenderer do
     end
     before :each do
       @atts = {:title => 'My Quiz', :points => 20, :num_questions => 5} 
-      @quiz = double('quiz', @atts.merge(:questions => []))
+      #@quiz = double('quiz', @atts.merge(:questions => []))
     end
     %w(title total_points num_questions).each do |var|
       it "should set '#{var}'" do
         value = @atts[var]
-        HtmlFormRenderer.new(@quiz, 't' => write_template("#{var}: <%= #{value} 
+        HtmlFormRenderer.new(Quiz.new(''), 't' => write_template("#{var}: <%= #{value} 
 %>")).render_quiz.output.
           should match /#{var}: #{value}/
       end
     end
   end
-=end
+  
   describe 'rendering raw content' do
     before :each do
       @q = MultipleChoice.new '<tt>xx</tt>', :raw => true
